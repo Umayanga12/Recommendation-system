5b464bee7d033ed8e0b30acb47ef83221fd5c893
Umayanga12 <umayangaathapaththu05@gmail.com>
2024-02-25 21:21:08 +0530

    retrieval part

---
+++ ranking.py
@@ -0,0 +1,24 @@
+import os 
+import pprint
+import tempfile
+import numpy as np 
+import pandas as pd 
+import tensorflow as tf 
+import keras 
+import tensorflow_datasets as tfds
+import tensorflow_recommenders as tfrs
+from typing import Dict, Text
+from keras import Sequential
+
+class LawyerSelectionModel:
+    def __init__(self) -> None:
+       pass
+   
+
+class LawyerSelection:
+    def __init__(self) -> None:
+        pass
+    
+
+embedding_diametions = 6
+
